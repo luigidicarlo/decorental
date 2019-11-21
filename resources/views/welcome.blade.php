7dcf6021d6bb2c4c4a48763241c491a964123710
GORONLINK <jorge_chourio@hotmail.com>
2019-11-21 14:21:28 -0400

    Propuesta en Inicio

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -64,6 +64,25 @@
         </div>
     </div>
 </section>
+
+
+<div class="jumbotron parent text-center" style="color: rgb(0, 0, 0);">
+    <div class="background"></div>
+    <div class="content my-auto">
+        <div class="content-no-flex">
+            <h1 class="text-white title-header">DECORENTAL</h1>
+            <p class="lead hero-description text-white">
+                LOREM IPSUM DOLOR SIT AMET CONSECTETUR
+            </p>
+            <p class="lead text-white">
+                <a href="#contact" class="btn btn-primary btn-lg">
+                    Empieza ya
+                </a>
+            </p>
+        </div>
+    </div>
+</div>
+
 <!-- ESTE CÓDIGO ES SOLO UNA PRUEBA -->
 @if ($products->count())
 
